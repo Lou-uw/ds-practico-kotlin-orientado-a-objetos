6c5f777bb89026ce90441370b358864bc459aec2
Lou-uw <lourdessoto@etec.uba.ar>
2026-05-06 13:08:37 -0300

    terminado

--- programacion-orientada-a-objetos-en-kotlin/src/main/kotlin/CuentaBancaria.kt
+++ programacion-orientada-a-objetos-en-kotlin/src/main/kotlin/CuentaBancaria.kt
@@ -1,12 +1,12 @@
 class CuentaBancaria(val titular: String, var saldo: Double) {
     fun depositar(monto: Double) {
         if (monto > 0) {
-            saldo += monto
+            this.saldo += monto
         }
     }
     fun retirar(monto: Double): Boolean {
-    return if (monto <= saldo) {
-        saldo -= monto
+    return if (monto <= this.saldo) {
+        this.saldo -= monto
         true
     } else {
         false
